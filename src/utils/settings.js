4e0468073a731064bb1354c79c82bc09efb17716
acmve <acmverden@hotmail.com>
2022-11-23 09:45:50 +0100

    droplet url with / at the end

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,8 +1,9 @@
-export const DROPLET_URL = "/CA2"
+export const DROPLET_URL = "/CA2/"
 
 export const BASE_URL =  "https://acmverden.dk/tomcat/CA2"
 export const LOGIN_URL = BASE_URL + "/api/login"
 export const VERIFY_URL = BASE_URL + "/api/verify"
+
 
 
 // https://acmverden.dk/tomcat/CA2 not working with BASE URL. POST WRONG when trying to log in
